65d8ffd13b49dc3e482610fccc410667c1880ac6
vahid <vahid.ostovarim@gmail.com>
2024-12-15 23:58:07 +0330

    change to models, add Doctor and Schedule model

---
+++ ShowQ/static/ShowQ/script.js
@@ -0,0 +1,9 @@
+function collapsfunc() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight){
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  } 
+}
