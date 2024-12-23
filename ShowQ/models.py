192d3b40de42dc05ca990a450d7d755b63837219
vahid <vahid.ostovarim@gmail.com>
2024-12-24 01:00:49 +0330

    removing unnecessary code

--- ShowQ/models.py
+++ ShowQ/models.py
@@ -21,12 +21,3 @@ class Profile(models.Model):
 
     def __self__(self):
         return self.User.username
-
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
