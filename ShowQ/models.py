68c1d4a391d0333ed8434baf06ec2b14131eeb6d
vahid <vahid.ostovarim@gmail.com>
2024-12-25 00:10:11 +0330

    synchoronise with main branche

--- ShowQ/models.py
+++ ShowQ/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+BOOKING_PERIOD = (
+    ("5", "5 دقیقه"),
+    ("10", "10 دقیقه"),
+    ("15", "15 دقیقه"),
+    ("20", "20 دقیقه"),
+    ("25", "25 دقیقه"),
+    ("30", "30 دقیقه"),
+    ("35", "35 دقیقه"),
+    ("40", "40 دقیقه"),
+    ("45", "45 دقیقه"),
+)
+
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -21,3 +34,33 @@ class Profile(models.Model):
 
     def __self__(self):
         return self.User.username
+
+class Doctor(models.Model):
+
+    name = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Schedule(models.Model):
+
+    doctor = models.ForeignKey(Doctor,on_delete=models.PROTECT)
+    start_date = models.DateField()
+    start_time = models.TimeField()
+    end_date = models.DateField()
+    end_time = models.TimeField()
+    days_of_week = models.BooleanField()
+    service_time = models.CharField(max_length=3, default="30", choices=BOOKING_PERIOD)
+
+    def __str__(self):
+        return self.doctor.name
+
+class AppointmentsModel(models.Model):
+
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    doctor = models.ForeignKey(to=Doctor,on_delete=models.PROTECT)
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.created_by.username
